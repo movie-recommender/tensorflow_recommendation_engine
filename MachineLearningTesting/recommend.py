814f47ffd53f4faab44584e15115a2343a72b79b
Ignatius Julian Kristandi <juliankristandi@yahoo.com>
2020-05-13 20:52:48 +0800

    recommendation done

--- MachineLearningTesting/recommend.py
+++ MachineLearningTesting/recommend.py
@@ -1,34 +1,38 @@
-import pandas as pd
+# Import libraries
 import numpy as np
-import warnings
+import pandas as pd
 
-warnings.filterwarnings('ignore')
+# Reading ratings file
+ratings = pd.read_csv('user_likes.csv', sep=',', names=['userId','movieId','rating'])
 
-# Reading the files 
-df = pd.read_csv('user_likes.csv', sep=',', names=['user_id','movie_id','swipe']) # ratings file
-movie_titles = pd.read_csv('movie_titles.csv', sep=',', names=['movie_id','movie_title']) # movie file
+# Reading movies file
+movies = pd.read_csv('movie_titles.csv', sep=',', names=['movieId','title'])
 
-# Merge databases on rated movies
-df = pd.merge(df, movie_titles, on='movie_id')
+movies.head()
 
-# Average rating and number of swipes per movie
-swipes = pd.DataFrame(df.groupby('movie_title')['swipe'].mean())
-swipes['number_of_swipes'] = df.groupby('movie_title')['swipe'].count()
+ratings.head()
 
-# Constructing the matrix, 2 = Like, 1 = Don't like, 0 = Not rated
-movie_matrix = df.pivot_table(index='user_id', columns='movie_title', values='swipe').fillna(0)
+n_users = ratings.userId.unique().shape[0]
+n_movies = ratings.movieId.unique().shape[0]
 
-# Top 10 rated movies
-swipes.sort_values('number_of_swipes', ascending=False).head(10)
 
-user_swipes_mean = np.mean(movie_matrix, axis = 1)
-swipes_demeaned = movie_matrix - user_swipes_mean.values.reshape(-1, 1)
+Ratings = ratings.pivot(index = 'userId', columns ='movieId', values = 'rating').fillna(0)
+Ratings.head()
+
+R = Ratings.values
+user_ratings_mean = np.mean(R, axis = 1)
+Ratings_demeaned = R - user_ratings_mean.reshape(-1, 1)
+
+
+sparsity = round(1.0 - len(ratings) / float(n_users * n_movies), 3)
 
 from scipy.sparse.linalg import svds
-U, sigma, Vt = svds(swipes_demeaned, k = 50)
+U, sigma, Vt = svds(Ratings_demeaned, k = 50)
+
 sigma = np.diag(sigma)
-all_user_predicted_swipes = np.dot(np.dot(U, sigma), Vt) + user_swipes_mean.values.reshape(-1, 1)
-preds = pd.DataFrame(all_user_predicted_swipes, columns = movie_matrix.columns)
+
+all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
+preds = pd.DataFrame(all_user_predicted_ratings, columns = Ratings.columns)
 
 
 def recommend_movies(predictions, userID, movies, original_ratings, num_recommendations):
@@ -38,19 +42,18 @@ def recommend_movies(predictions, userID, movies, original_ratings, num_recommen
     sorted_user_predictions = preds.iloc[user_row_number].sort_values(ascending=False) # User ID starts at 1
     
     # Get the user's data and merge in the movie information.
-    user_data = original_ratings[original_ratings.user_id == (userID)]
-    user_full = (user_data.merge(movies, how = 'left', left_on = 'movie_id', right_on = 'movie_id').
-                     sort_values(['swipe'], ascending=False)
+    user_data = original_ratings[original_ratings.userId == (userID)]
+    user_full = (user_data.merge(movies, how = 'left', left_on = 'movieId', right_on = 'movieId').
+                     sort_values(['rating'], ascending=False)
                  )
+    print('User {} has already rated {} movies.'.format(userID, user_full.shape[0]))
+    print('Recommending highest {} predicted ratings movies not already rated.'.format(num_recommendations))
 
-    print ('User {0} has already rated {1} movies.'.format(userID, user_full.shape[0]))
-    print ('Recommending highest {0} predicted ratings movies not already rated.'.format(num_recommendations))
-    
     # Recommend the highest predicted rating movies that the user hasn't seen yet.
-    recommendations = (movies[~movies['movie_id'].isin(user_full['movie_id'])].
+    recommendations = (movies[~movies['movieId'].isin(user_full['movieId'])].
          merge(pd.DataFrame(sorted_user_predictions).reset_index(), how = 'left',
-               left_on = 'movie_id',
-               right_on = 'movie_id').
+               left_on = 'movieId',
+               right_on = 'movieId').
          rename(columns = {user_row_number: 'Predictions'}).
          sort_values('Predictions', ascending = False).
                        iloc[:num_recommendations, :-1]
@@ -58,5 +61,7 @@ def recommend_movies(predictions, userID, movies, original_ratings, num_recommen
 
     return user_full, recommendations
 
-already_rated, predictions = recommend_movies(preds, 1, movie_titles, df, 20)
 
+already_rated, predictions = recommend_movies(preds, 50, movies, ratings, 20)
+
+print(predictions.head(20))
